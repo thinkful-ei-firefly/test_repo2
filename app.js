6247fbd71d169d81c024e179c961155afd5b14c0
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-07-08 14:25:49 -0400

    solve drill

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-/* eslint-disable strict */
 function getYearOfBirth(age) {
     if (age < 0) {
         throw new Error('Age cannot be negative.');
@@ -7,19 +6,23 @@ function getYearOfBirth(age) {
 }
 
 function createGreeting(name, age) {
-    if (name === undefined || age === undefined ) {
+    if (name === undefined || age === undefined) {
         throw new Error('Arguments not valid.');
-    }  else {
-        return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${getYearOfBirth(age)}.`;
-}
+    } else if (typeof name !== 'string') {
+        throw new TypeError('Name must be a string.');
+    } else if (typeof age !== 'number') {
+        throw new TypeError('Age must be a number.');
+    }
+    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${getYearOfBirth(age)}.`;
 }
 // console.log(createGreeting());
 
 const greeting1 = createGreeting('Ben', 30);
+// eslint-disable-next-line no-console
 console.log(greeting1);
 
 try {
-    const greeting1 = createGreeting('Ben', -10);
+    const greeting1 = createGreeting('Ben', -50);
 } catch(e) {
     console.log(`${e.name}: ${e.message}`);
 }
