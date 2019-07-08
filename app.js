30c02e31a0a17cbefd8e9216a21c729fbb07ccba
Ben Rosen <benjamin.j.rosen@gmail.com>
2019-07-08 13:55:00 -0400

    return function and invoke

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 /* eslint-disable strict */
 function createGreeting() {
-    console.log('Hi, my name is Ben and I\'m 30 years old.');
+    return 'Hi, my name is Ben and I\'m 30 years old.';
 }
+// console.log(createGreeting());
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
